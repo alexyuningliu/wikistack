aa66740aa31a860b83cafa88723baf28802aadb0
Alex Liu <yuningalexliu@gmail.com>
2015-03-07 10:39:46 -0500

    able to post to db

--- routes/add.js
+++ routes/add.js
@@ -3,12 +3,37 @@ var router = express.Router();
 
 /* GET the ADD form. */
 router.get('/', function(req, res) {
-  res.send('respond with a resource');
+  res.render('add', {title: "ADD A PAGE"});
 });
 
 /* POST a form. */
 router.post('/submit', function(req, res) {
-  res.send('respond with a resource');
+  var generateUrlName = function(name) {
+    if (typeof name != "undefined" && name !== "") {
+      // Removes all non-alphanumeric characters from name
+      // And make spaces underscore
+      return name.replace(/\s/ig,"_").replace(/\W/ig,"");
+    } else {
+      // Generates random 5 letter string
+      return Math.random().toString(36).substring(2,7);
+    }
+  };
+
+  var models = require('../models/');
+
+  // STUDENT ASSIGNMENT:
+  // add definitions of the `title`, `body` and `url_name` variables here
+
+  console.log(req.body.title);
+  console.log(req.body.body);
+
+  var title = req.body.title;
+  var body = req.body.body;
+  var url_name = generateUrlName(req.body.title);
+
+  var p = new models.Page({ "title": title, "body": body, "url_name": url_name });
+  p.save();
+  res.redirect('/');
 });
 
 module.exports = router;
