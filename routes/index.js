d5f8a8c039cc75af1d86f32175b735331f659edb
Alex Liu <yuningalexliu@gmail.com>
2015-03-07 11:13:51 -0500

    got posts to display from the DB and added filter

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('index', { title: 'BROWSE MY WIKISTACK' });
+	var models = require('../models/');
+	var docs = models.Page.find(function (err, docs) {
+		if (err) return console.error(err);
+		console.log(docs);
+		res.render('index', { title: 'BROWSE MY WIKISTACK', docs: docs });
+	});
 });
 
 module.exports = router;
